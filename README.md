e9d5002caf8e5a35ec7db7386a0dfbb3e3c2e450
htavenido <hec.odesk@gmail.com>
2013-03-13 16:48:17 +0800

    updates

--- README.md
+++ README.md
@@ -10,5 +10,20 @@ Minimal setup for mock
 
 [note: grunt-css issue](https://github.com/jzaefferer/grunt-css/issues/18)
 
-Requires node 0.8.*
+Requires node 0.8.* or higher
+
+
+# Usage
+
+1. Install [node](http://nodejs.org/)
+2. Install [nvm](https://github.com/creationix/nvm)
+3. Install [sass](http://sass-lang.com/download.html)
+2. git clone https://github.com/htavenido/html5-boilerplate-min/ YOUR-DIR-NAME
+3. cd YOUR-DIR-NAME
+4. If your node.js version is < 0.8, type: nvm use 0.8
+5. npm install
+6. open a new tab, then type: cd css/src/
+7. sass --watch main.scss:main.css
+8. on the previous tab, type grunt
+
 
--- grunt.js
+++ grunt.js
@@ -1,28 +1,12 @@
 /*global module:false*/
 module.exports = function (grunt) {
-    var SRC_CSS = "./css/src",
-        BUILD_CSS = "./css/";
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             css: {
-                src: ["css/src/normalize.css",'css/src/main.css'],
+                src: ['css/src/normalize.css','css/src/main.css'],
                 dest: "css/all.css"
             }
-        },
-        csslint: {
-            base_theme: {
-                src: "css/all.min.css",
-                rules: {
-                    "import": false,
-                    "overqualified-elements": 2
-                }
-            }
-        },
-        watch: {
-            files: "css/src/*.css",
-            tasks: ["concat"]
         }
     });
 
