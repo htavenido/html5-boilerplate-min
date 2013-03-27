5cb9f2509af6efc9539ce0a0c85a6bae2d1404c1
htavenido <hec.odesk@gmail.com>
2013-03-27 15:00:11 +0800

    add mvc setup, livereload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-
+require.config({
+  paths: {
+    jquery: 'libs/jquery-1.9.0.min',
+    underscore: 'libs/underscore-min',
+    backbone: 'libs/backbone-min',
+    app: 'app'
+  },
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    }
+  }
+});
